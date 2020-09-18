be15939f324fbd894f51c8531e981d7f87cf1867
jviguy <jviguytwo2@gmail.com>
2020-09-18 19:56:51 -0400

    TIME BANS = FINISHEDDDD

--- SharperBot/Commands/Modules/Moderation/TimeBanCommand.cs
+++ SharperBot/Commands/Modules/Moderation/TimeBanCommand.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 
 namespace SharperBot.Commands.Modules.Moderation
@@ -6,9 +8,24 @@ namespace SharperBot.Commands.Modules.Moderation
     public class TimeBanCommand : ModuleBase<SocketCommandContext>
     {
         [Command("timeban", RunMode = RunMode.Async)]
-        public async Task TimeBanAsync()
+        public async Task TimeBanAsync(string user,int day,int hour,int minutes,int seconds)
         {
-            
+            var id = MentionUtils.ParseUser(user);
+            var ban = Context.Guild.GetUser(id);
+            await Context.Message.AddReactionAsync(new Emoji("✅"));
+            var ts = new TimeSpan(day,hour,minutes,seconds);
+            await Task.Run(async () =>
+            {
+                try
+                {
+                    await Task.Delay((int) ts.TotalMilliseconds);
+                }
+                catch (Exception e)
+                {
+                    await ReplyAsync(e.Message);
+                }
+                await ReplyAsync("DONE");
+            });
         }
     }
 }
