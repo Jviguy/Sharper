0db8f203f373618d4c1dbf77297aa269ea0d2472
jviguy <jviguytwo2@gmail.com>
2020-09-18 21:26:21 -0400

    Added the MessageRepeater.cs it repeats

---
+++ SharperBot/Commands/Modules/Misc/MessageRepeater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+using Discord;
+using Discord.Commands;
+
+namespace SharperBot.Commands.Modules.Misc
+{
+    [RequireUserPermission(GuildPermission.Administrator)]
+    public class MessageRepeater : ModuleBase<SocketCommandContext>
+    {
+        [Command("msgrepeat", RunMode = RunMode.Async)]
+        public async Task MsgRepeatAsync(int day, int hour, int minutes, int seconds, [Remainder] string message)
+        {
+            var tm = new Timer();
+            tm.Elapsed += async (Object source, ElapsedEventArgs e) =>
+            {
+                await ReplyAsync(message);
+            };
+            tm.Interval = new TimeSpan(day,hour,minutes,seconds).TotalMilliseconds;
+            tm.Start();
+        }
+    }
+}
