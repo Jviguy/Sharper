0310817531a80a7e01020c76b3a5b60eaaa25bb3
jviguy <jviguytwo2@gmail.com>
2020-09-18 23:08:10 -0400

    Added PpSizeCommand.cs for fun

---
+++ SharperBot/Commands/Modules/Misc/PpSizeCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace SharperBot.Commands.Modules.Misc
+{
+    public class PpSizeCommand : ModuleBase<SocketCommandContext>
+    {
+        private readonly Random rnd = new Random();
+        [Command("pp",false,RunMode = RunMode.Async)]
+        public async Task PpSizeAsync()
+        {
+            var size = rnd.Next(1000);
+            var pp = new StringBuilder(string.Empty);
+            pp.Append("8");
+            foreach (var i in Enumerable.Range(0, size))
+            {
+                pp.Append("=");
+            }
+            pp.Append(">");
+            await ReplyAsync(embed: new EmbedBuilder()
+            {
+                Title = "Nice Cock Bro!",
+                Description = pp.ToString()
+            }.Build());
+        }
+    }
+}
