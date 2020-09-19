a9556775bb0b043b05d68ceb08480e0e7cd22713
jviguy <jviguytwo2@gmail.com>
2020-09-19 15:47:35 -0400

    ADded in the webhooking error handling

--- SharperBot/Commands/Modules/Misc/WebHookCommand.cs
+++ SharperBot/Commands/Modules/Misc/WebHookCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Discord;
 using Discord.Commands;
 
 namespace SharperBot.Commands.Modules.Misc
@@ -13,20 +14,33 @@ namespace SharperBot.Commands.Modules.Misc
         [Command("webhook", RunMode = RunMode.Async)]
         public async Task CurlAsync(string url,[Remainder] string request)
         {
-            using (var webClient = new WebClient())
+            try
             {
-                var re = request.Split(" ");
-                var l = re.ToList();
-                for (var a = 0; a < 3; a++)
+                using (var webClient = new WebClient())
                 {
-                    l.RemoveAt(0);
+                    var re = request.Split(" ");
+                    var l = re.ToList();
+                    for (var a = 0; a < 3; a++)
+                    {
+                        l.RemoveAt(0);
+                    }
+
+                    var msg = string.Join(" ", l);
+                    webClient.UploadValues(new Uri(url), new NameValueCollection()
+                    {
+                        {re[0], re[1]},
+                        {re[2], msg}
+                    });
                 }
-                var msg = string.Join(" ", l);
-                webClient.UploadValues(new Uri(url),new NameValueCollection()
+            }
+            catch (Exception e)
+            {
+                await ReplyAsync(embed: new EmbedBuilder()
                 {
-                    {re[0], re[1]},
-                    {re[2],msg}
-                });
+                    Title = "Error",
+                    Description = e.Message,
+                    ImageUrl = "https://media.tenor.com/images/21aa6ef4312e1abcd50ffca5e1d4dd75/tenor.gif"
+                }.Build());
             }
         }
     }
