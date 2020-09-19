a038d96986ead9785980bb6ecf1701025aa1d7d8
jviguy <jviguytwo2@gmail.com>
2020-09-18 20:23:34 -0400

    Added a misc LoadingCommand.cs for a funny embed

---
+++ SharperBot/Commands/Modules/Misc/LoadingCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using SharperBot.Services.Discord.Embed;
+
+namespace SharperBot.Commands.Modules.Misc
+{
+    public class LoadingCommand : ModuleBase<SocketCommandContext>
+    {
+        private LoadingUtils loading = new LoadingUtils();
+        [Command("loading", RunMode = RunMode.Async)]
+        public async Task LoadingAsync()
+        {
+            await ReplyAsync(embed: new EmbedBuilder()
+            {
+                Title = Context.User.Username + " Is Loading Please Hold!",
+                ImageUrl = loading.FetchRandomGIF()
+            }.Build());
+        }
+    }
+}
