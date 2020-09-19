0c3d2db29213c1cb674ac23f9995f3c2ab884586
jviguy <jviguytwo2@gmail.com>
2020-09-19 12:30:04 -0400

    Added in curl for webhooks and random shit idk

---
+++ SharperBot/Commands/Modules/Misc/cURLCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace SharperBot.Commands.Modules.Misc
+{
+    public class cURLCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("curl", RunMode = RunMode.Async)]
+        public async Task CurlAsync(string url,[Remainder] string request)
+        {
+            var array = request.Split(" ");
+            var requestContent = new FormUrlEncodedContent(new [] {
+                new KeyValuePair<string, string>(array[0],string.Join(" ",array,1)),
+            });
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.PostAsync(
+                url,requestContent);
+            HttpContent responseContent = response.Content;
+            using (var reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+            {
+                await ReplyAsync(embed: new EmbedBuilder()
+                {
+                    Title = "Curl Response!",
+                    Description = await reader.ReadToEndAsync()
+                }.Build());
+            }
+        }
+    }
+}
