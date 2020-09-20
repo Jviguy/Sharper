0e41d418a4cfb9b01e66cf42f353055a4140ccf7
jviguy <jviguytwo2@gmail.com>
2020-09-20 02:38:56 -0400

    Cleaning up code

---
+++ SharperBot/Commands/Modules/Misc/UserInfoCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace SharperBot.Commands.Modules.Misc
+{
+    public class UserInfoCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("usrinfo", RunMode = RunMode.Async)]
+        public async Task UserInfoAsync(string id)
+        {
+            try
+            {
+                SocketGuildUser user;
+                try
+                {
+                    var b = MentionUtils.ParseUser(id);
+                    user = Context.Guild.GetUser(b);
+                }
+                catch (Exception)
+                {
+                    user = Context.Guild.GetUser(Convert.ToUInt64(id));
+                }
+                await ReplyAsync(embed: new EmbedBuilder()
+                {
+                    Title = "User Info On " + user.Username,
+                }
+                    .AddField("Username: ",user.Username)
+                    .AddField("Nickname: ",user.Nickname ?? "None")
+                    .AddField("Joined This Server at: ",user.JoinedAt.HasValue ? user.JoinedAt.Value.Date.ToString() : "Date Not Found")
+                    .AddField("Joined Discord on: ",user.CreatedAt.DateTime.Date.ToString())
+                    .Build());
+            }
+            catch (Exception e)
+            {
+                await ReplyAsync(embed: new EmbedBuilder()
+                {
+                    Title = "ERROR!",
+                    Description = e.Message 
+                }.Build());
+            }
+        }
+    }
+}
