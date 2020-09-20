054db120880d9116c97401525752454b245356bc
jviguy <jviguytwo2@gmail.com>
2020-09-20 00:01:17 -0400

    Added in AvatarCommand.cs for things

---
+++ SharperBot/Commands/Modules/Misc/AvatarCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace SharperBot.Commands.Modules.Misc
+{
+    public class AvatarCommand : ModuleBase<SocketCommandContext>
+    {
+        [Command("av", RunMode = RunMode.Async)]
+        public async Task AvatarAsyc(string id)
+        {
+            SocketGuildUser user;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            try
+            {
+                var b = MentionUtils.ParseUser(id);
+                user = Context.Guild.GetUser(b);
+            }
+            catch (Exception)
+            { 
+                user = Context.Guild.GetUser(Convert.ToUInt64(id));
+            }
+
+            await ReplyAsync(embed: new EmbedBuilder()
+            {
+                Title = user.Username + "'s Avatar!",
+                ImageUrl = user.GetAvatarUrl(),
+                Footer = new EmbedFooterBuilder()
+                {
+                    Text = $" - Requested by {Context.User.Username} - Done in 0.{stopwatch.ElapsedMilliseconds}s!"
+                }
+            }.Build());
+            stopwatch.Stop();
+        }
+    }
+}
